9f0d3248bde84836e9677acafb23f90db18e7510
DuyDo24 <a1921156@adelaide.edu.au>
2025-08-05 15:29:37 +0930

    f

--- week1/StreetParking/StreetParking.hpp
+++ week1/StreetParking/StreetParking.hpp
@@ -6,16 +6,13 @@ public:
     int freeParks(string street){
         int count = 0;
         for (int i = 0; i < street.size(); i++){
-            if (street[i] != '-') {
-                continue; 
-            }
             if (street[i] == 'D' || street[i] == 'B' || street[i] == 'S'){
                 continue;
             } 
             if (i - 1 >= 0 && (street[i - 1] == 'B' || street[i - 1] == 'S')) {
                 continue;
             }
-            if (i - 2 >= 0 && street[i - 2] == 'B') {
+            if (i + 2 >= 0 && street[i - 2] == 'B') {
                 continue;
             }
             if (i + 1 < street.size() && street[i + 1] == 'S') {
