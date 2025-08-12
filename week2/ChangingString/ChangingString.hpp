69bbdb7e3c6745a3888181baa1bf40649b85b2ac
DuyDo24 <a1921156@adelaide.edu.au>
2025-08-12 17:23:09 +0930

    yo

--- week2/ChangingString/ChangingString.hpp
+++ week2/ChangingString/ChangingString.hpp
@@ -1,2 +1,29 @@
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+class ChangingString{
+public:
+    int distance(string A, string B, int K){
+        int size = A.size();
+        vector<int> diff(size);
+
+        for (int i = 0; i < size; i++){
+            diff[i] = abs(A[i] - B[i]);
+        }
+        //sort from descneding order
+        sort(diff.begin(), diff.end(), greater<int>());
+
+        //find K biggest elements in diff
+        for (int i = 0; i < K; i++){
+            diff[i] = 0;
+        }
+
+        int result = 0;
+        for (int d : diff){
+            result += d;
+        }
+        return result;
+    }
+};
