29bcff6373d9c99ee500aa3b692708af769a5350
DuyDo24 <a1921156@adelaide.edu.au>
2025-08-13 19:21:05 +0930

    f

--- week2/AlternateColors/AlternateColors.hpp
+++ week2/AlternateColors/AlternateColors.hpp
@@ -17,32 +17,30 @@ public:
         r -= minimum;     
         g -= minimum;      
         b -= minimum;  
-        
-        int colors_left = 0;
-        if (r > 0) colors_left++;
-        if (g > 0) colors_left++;
-        if (b > 0) colors_left++;
-        
-        if (colors_left == 2) {
-            // Two colors remaining - they alternate
-            if (r == 0) {
-                // Only green and blue left, pattern: GREEN, BLUE, GREEN, BLUE...
-                if ((k - 1) % 2 == 0) return "GREEN";
-                return "BLUE";
-            } else if (g == 0) {
-                // Only red and blue left, pattern: RED, BLUE, RED, BLUE...
-                if ((k - 1) % 2 == 0) return "RED";
-                return "BLUE";
-            } else {
-                // Only red and green left, pattern: RED, GREEN, RED, GREEN...
-                if ((k - 1) % 2 == 0) return "RED";
-                return "GREEN";
-            }
-        } else {
-            // Only one color remaining
-            if (r > 0) return "RED";
-            if (g > 0) return "GREEN";
+
+        if (r > 0 && g > 0) {
+            if (k % 2 == 1) return "RED";
+            return "GREEN";
+        }
+
+        if (r > 0 && b > 0) {
+            if (k % 2 == 1) return "RED";
             return "BLUE";
         }
+
+        if (g > 0 && b > 0) {
+            if (k % 2 == 1) return "GREEN";
+            return "BLUE";
+        }
+
+        if (r > 0) {
+            return "RED";
+        }
+
+        if (g > 0) {
+            return "GREEN";
+        }
+  
+        return "BLUE";
     }
 };
