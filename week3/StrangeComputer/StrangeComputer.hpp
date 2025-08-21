ea1bd19a046cf73c0326faea30b9c78e93df08c0
DuyDo24 <a1921156@adelaide.edu.au>
2025-08-21 20:08:56 +0930

    f

--- week3/StrangeComputer/StrangeComputer.hpp
+++ week3/StrangeComputer/StrangeComputer.hpp
@@ -5,15 +5,11 @@ class StrangeComputer{
 public:
     int setMemory(string mem){
         int count = 0;
-        bool num = false;
+        char num = '0';
         for (int i = 0; i < mem.size(); i++){
             if (mem[i] != num ){
                 count++;
-                if (num == true){
-                    num == false;
-                } else {
-                    num == true;
-                }
+                num = mem[i];
             }
         }
         return count;
