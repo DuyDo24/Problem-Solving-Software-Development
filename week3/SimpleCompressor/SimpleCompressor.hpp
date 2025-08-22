395f7197ed2f21e264d89fb3d6544981aaae945d
DuyDo24 <a1921156@adelaide.edu.au>
2025-08-22 20:02:31 +0930

    f

---
+++ week3/SimpleCompressor/SimpleCompressor.hpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class SimpleCompressor{
+public:
+    string helper(const string& data, int &pos){
+        string result = "";
+        while (pos < data.size() && data[pos] != ']'){
+            if (isalpha(data[pos])){
+                result.push_back(data[pos]);
+                pos++;
+            }
+            else if (isdigit(data[pos])){
+                int D = data[pos] - '0';
+                pos += 2;
+                string inside = helper(data, pos);
+                pos++;
+                for (int i = 0; i < D; i++){
+                    result += inside;
+                }
+            }
+            
+        }
+        return result;
+    }
+
+    string uncompress(string data){
+        int pos = 0;
+        return helper(data, pos);
+    }
+};
