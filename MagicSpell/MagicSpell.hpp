307ab2ef1aa5a9bf183f8427fb08f55e1dbbc440
DuyDo24 <a1921156@adelaide.edu.au>
2025-08-23 21:25:47 +0930

    f

--- MagicSpell/MagicSpell.hpp
+++ MagicSpell/MagicSpell.hpp
@@ -7,16 +7,19 @@ public:
     string fixTheSpell(string spell){
         int l = 0;
         int r = spell.size() - 1;
-        while (l < r){
-            if ((spell[l] == 'A' || spell[l] == 'Z') && (spell[r] == 'A' || spell[r] == 'Z')){
-                swap(spell[l],spell[r]);
-            }
-            if (spell[l] != 'A' && spell[l] != 'Z'){
+        while (l < r) {
+            if (!(spell[l] == 'A' || spell[l] == 'Z')) {
                 l++;
+                continue;
             }
-            if (spell[r] != 'A' && spell[r] != 'Z'){
+            if (!(spell[r] == 'A' || spell[r] == 'Z')) {
                 r--;
+                continue;
             }
+            // both are A or Z
+            swap(spell[l], spell[r]);
+            l++;
+            r--;
         }
         return spell;
     }
