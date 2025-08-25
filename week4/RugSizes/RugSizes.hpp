2e625ab5383d893ee94a2d9542f65f6bcd94da42
DuyDo24 <a1921156@adelaide.edu.au>
2025-08-25 21:45:43 +0930

    f

--- week4/RugSizes/RugSizes.hpp
+++ week4/RugSizes/RugSizes.hpp
@@ -4,7 +4,7 @@ class RugSizes{
 public:
     int rugCount(int area){
         int count = 0;
-        for (int i = 1; i * i < area;i++){
+        for (int i = 1; i * i <= area;i++){
             if (area % i != 0) continue;
             int length = area / i;
             if (i % 2 == 0 && length % 2 == 0 && i != length) continue;
