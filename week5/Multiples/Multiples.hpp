d22d616ccb4d7ec9c7422fba18e276cc0d4870b1
DuyDo24 <a1921156@adelaide.edu.au>
2025-08-31 18:29:30 +0930

    f

--- week5/Multiples/Multiples.hpp
+++ week5/Multiples/Multiples.hpp
@@ -1,16 +1,12 @@
 class Multiples{
 public:
     int number(int min, int max, int factor){
-        int high;
-        if (high < 0){
-            high = (max + (factor-1) /factor);
-        } else {
-            high = max / factor;
-        }
+        int high = max/factor;
 
         int low;
         if (min < 0) {
             low = min/factor;
+            if (min % factor == 0 ){low--;}
         } else {
             low = (min + (factor-1))/factor;
         }
