dcaa947a07ff5ddda7d5b8c72309aab6c415d3e2
DuyDo24 <a1921156@adelaide.edu.au>
2025-08-31 22:58:44 +0930

    f

--- week5/Multiples/Multiples.hpp
+++ week5/Multiples/Multiples.hpp
@@ -1,10 +1,12 @@
 #include <cmath>
 using namespace std;
+
 class Multiples {
 public:
-   int number(int min, int max, int factor) {
-        int high = max / factor;  
-        int low = (int)floor((double)(min - 1) / factor);
-        return high - low;
+    int number(int min, int max, int factor) {
+        int maxDiv = (int)floor((double)max / factor);
+        int minDiv = (int)floor((double)(min - 1) / factor);
+        
+        return maxDiv - minDiv;
     }
 };
