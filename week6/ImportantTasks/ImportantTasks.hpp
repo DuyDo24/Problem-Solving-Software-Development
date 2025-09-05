d0493790c604209f72e67f1263b28d4c2016ec48
DuyDo24 <a1921156@adelaide.edu.au>
2025-09-05 18:20:06 +0930

    f

--- week6/ImportantTasks/ImportantTasks.hpp
+++ week6/ImportantTasks/ImportantTasks.hpp
@@ -5,14 +5,14 @@ using namespace std;
 class ImportantTasks{
 public:
     int maximalCost(vector<int> complexity, vector<int> computers){
-        sort(computers.begin(), computers.end());
-        sort(complexity.begin(), complexity.end());
+        sort(computers.rbegin(), computers.rend());
+        sort(complexity.rbegin(), complexity.rend());
 
         int count = 0;
         int complex = 0;
         int comput = 0;
 
-        while (complex < complexity.size() || comput < computers.size()){
+        while (complex < complexity.size() && comput < computers.size()){
             if (computers[comput] >= complexity[complex]){
                 count++;
                 comput++;
