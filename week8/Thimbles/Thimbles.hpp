e311af97bffca2bc0634a59bf2fa7d0a50903fce
DuyDo24 <a1921156@adelaide.edu.au>
2025-10-03 18:00:04 +0930

    f

---
+++ week8/Thimbles/Thimbles.hpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Thimbles{
+public:
+    int thimbleWithBall(vector<string> swaps){
+        int current = 1;
+        for (string &c : swaps){
+            int x = c[0] - '0';
+            int y = c[3] - '0';
+            if (x == current) current = y;
+            if (y == current) current = x;
+        }
+        return current;
+    }
+};
