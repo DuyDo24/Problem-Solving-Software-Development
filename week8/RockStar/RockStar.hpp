2957637a942730d2a2eb2529f5360905601c935e
DuyDo24 <a1921156@adelaide.edu.au>
2025-10-09 20:14:56 +1030

    f

---
+++ week8/RockStar/RockStar.hpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+using namespace std;
+
+class RockStar{
+public:
+    int getNumSongs(int ff, int fs, int sf, int ss){
+        int total = 0;
+        if (ff == 0 & fs == 0){
+            if (sf >= 1){
+                return ss + 1;
+            } 
+            return ss;
+        }
+        total = ff;
+        if (fs != 0){
+            total++;
+            total += ss;
+            if (sf != 0){
+                fs--;
+                total += 2 * min(sf,fs);
+            }
+        }
+        return total;
+    }
+};
