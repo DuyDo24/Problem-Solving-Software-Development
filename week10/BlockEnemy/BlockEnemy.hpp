044db2ef5637bc2879f20c8fa425d007e021680c
DuyDo24 <a1921156@adelaide.edu.au>
2025-10-22 14:19:42 +1030

    f

--- week10/BlockEnemy/BlockEnemy.hpp
+++ week10/BlockEnemy/BlockEnemy.hpp
@@ -3,42 +3,56 @@
 #include <sstream>
 #include <utility>
 #include <algorithm>
+#include <cmath>
+
 using namespace std;
 
 class BlockEnemy {
 public:
     int N;
-    vector<vector<pair<int,int>>> adj;
+    vector<vector<pair<int,int>>> adj; 
     vector<bool> occupied;
 
-    pair<long long,bool> dfs(int u, int parent) {
-        bool hasOccupied = occupied[u];
-        long long cost = 0;
-        vector<int> occupiedEdges;
+    const int INF_EFFORT = 1000001; 
 
-        for (size_t i = 0; i < adj[u].size(); ++i) {
-            int v = adj[u][i].first;
-            int w = adj[u][i].second;
+    pair<long long,bool> dfs(int u, int parent) {
+        long long current_cost = 0;
+        vector<int> occupied_paths_weights;
+
+        for (const auto& edge : adj[u]) {
+            int v = edge.first;
+            int w = edge.second;
             if (v == parent) continue;
 
-            auto sub = dfs(v, u);
-            cost += sub.first;
-            if (sub.second)
-                occupiedEdges.push_back(w);
+            auto sub_result = dfs(v, u);
+            current_cost += sub_result.first;
+
+            if (sub_result.second) {
+                occupied_paths_weights.push_back(w);
+            }
         }
 
-        if (occupied[u]) hasOccupied = true;
-
-        if (occupiedEdges.size() > 1) {
-            sort(occupiedEdges.begin(), occupiedEdges.end());
-            for (size_t i = 0; i + 1 < occupiedEdges.size(); ++i)
-                cost += occupiedEdges[i];
-            hasOccupied = true;
-        } else if (occupiedEdges.size() == 1) {
-            hasOccupied = true;
+        if (occupied[u]) {
+            occupied_paths_weights.push_back(INF_EFFORT);
         }
 
-        return {cost, hasOccupied};
+        int k = occupied_paths_weights.size();
+        
+        if (k > 1) {
+            sort(occupied_paths_weights.begin(), occupied_paths_weights.end());
+
+            for (int i = 0; i < k - 1; ++i) {
+                current_cost += occupied_paths_weights[i];
+            }
+            
+            return {current_cost, true};
+
+        } else if (k == 1) {
+            return {current_cost, true};
+
+        } else {
+            return {current_cost, false};
+        }
     }
 
     int minEffort(int N, vector<string> roads, vector<int> occupiedTowns) {
@@ -46,8 +60,9 @@ public:
         adj.assign(N, {});
         occupied.assign(N, false);
 
-        for (int town : occupiedTowns)
+        for (int town : occupiedTowns) {
             occupied[town] = true;
+        }
 
         for (auto &s : roads) {
             stringstream ss(s);
