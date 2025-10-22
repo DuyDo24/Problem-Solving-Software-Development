2a4599cafa3b4dc395d35bf9a24c588289fe31ec
DuyDo24 <a1921156@adelaide.edu.au>
2025-10-22 23:43:22 +1030

    f

--- week10/BridgeSort/BridgeSort.hpp
+++ week10/BridgeSort/BridgeSort.hpp
@@ -4,31 +4,34 @@
 #include <string>
 using namespace std;
 
-class BridgeSort{
+class BridgeSort {
 public:
-    string sortedHand(string hand){
+    string sortedHand(string hand) {
         vector<pair<char, char>> cards;
-        for (int i = 0;i < hand.size()/2; i+2){
-            cards.push_back({hand[i],hand[i+1]});
+
+        for (int i = 0; i < (int)hand.size(); i += 2) {
+            cards.push_back({hand[i], hand[i + 1]});
         }
+
         string suit = "CDHS";
         string value = "23456789TJQKA";
 
-         auto suitRank = [&](char s) {
+        auto suitRank = [&](char s) {
             return suit.find(s);
         };
         auto valueRank = [&](char v) {
             return value.find(v);
         };
 
-        sort(cards.begin(), cards.end(), [&](auto &a, auto &b){
-            if (a.first() != b.first())
-                return suitRank(a.first) < suitRank(b.first());
+        sort(cards.begin(), cards.end(), [&](auto &a, auto &b) {
+            if (a.first != b.first)
+                return suitRank(a.first) < suitRank(b.first);
             return valueRank(a.second) < valueRank(b.second);
         });
 
+
         string result;
-        for (auto &c : cards){
+        for (auto &c : cards) {
             result += c.first;
             result += c.second;
         }
